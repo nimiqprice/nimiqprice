b047430e9b17058d9c1f725a5ecc80900a5ae832
Sebastian Almnes <sebastian.almnes@gmail.com>
2018-03-07 20:35:28 +0100

    Add price in BTC, ETH, market cap and price of NIM

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,6 +9,8 @@ function fetchNimiqPrice() {
       const percentChange24h = nimiqMarketInfo.percent_change_24h
       const percentChange1h  = nimiqMarketInfo.percent_change_1h
       const percentChange7d  = nimiqMarketInfo.percent_change_7d
+      const totalMarketCap   = nimiqMarketInfo.market_cap_usd
+      const priceBtc         = nimiqMarketInfo.price_btc
 
       document.getElementById('price-usd').textContent = `$${nimiqPriceUsd}`
       document.title = `($${nimiqPriceUsd}) Nimiq price - simple and mobile friendly`
@@ -18,6 +20,12 @@ function fetchNimiqPrice() {
         lastDay:  percentChange24h,
         lastWeek: percentChange7d
       })
+
+      document.getElementById('marketcap').textContent = prettyMarketCap(totalMarketCap)
+      document.getElementById('nim-value').textContent = `$${priceOfNim(nimiqPriceUsd)}`
+      document.getElementById('price-btc').textContent = `${priceBtc} BTC`
+
+      fetchEthPrice(nimiqPriceUsd)
     })
 }
 
@@ -35,6 +43,28 @@ function showPercentChange(percent, elementId) {
     percentChangeHeader.classList.add('negative')
   }
  percentChangeHeader.textContent = `${percent}%`
+}
+
+function prettyMarketCap(marketcap) {
+  const oneMillion          = 1000000
+  const marketCapInMillions = parseFloat(marketcap / oneMillion).toFixed(1)
+  return `$${marketCapInMillions} Million`
+}
+
+function priceOfNim(netPrice) {
+  return parseFloat(netPrice / 100.0).toFixed(3)
+}
+
+function fetchEthPrice(nimiqPriceUsd) {
+  fetch('https://api.coinmarketcap.com/v1/ticker/ethereum/')
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(ethTicker){
+      const ethMarketInfo    = ethTicker[0]
+      const priceOfNimiqInEth = parseFloat(nimiqPriceUsd / ethMarketInfo.price_usd).toFixed(7)
+      document.getElementById('price-eth').textContent = `${priceOfNimiqInEth} ETH`
+    })
 }
 
 fetchNimiqPrice()
